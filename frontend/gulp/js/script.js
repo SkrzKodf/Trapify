5d079ddfad8ab488501eec9beaa3689deb280cf4
Даша +_+ <dar.zhukova2014@mail.ru>
2024-06-26 17:30:31 +0300

    добавлены плейлист и название

--- frontend/gulp/js/script.js
+++ frontend/gulp/js/script.js
@@ -1,1 +1,44 @@
-alert(1)
+class Playlist extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    createElem({ playName, playID }) {
+
+        this.playName = playName;
+        this.playID = playID;
+
+        this.render();
+    }
+    render() {
+        const elem = document.createElement('div');
+        elem.classList.add('playCont');
+
+        const playImageEl = document.createElement('img');
+        playImageEl.classList.add('image');
+        elem.appendChild(playImageEl);
+
+        const playNameEl = document.createElement('div');
+        playNameEl.classList.add('name');
+        playNameEl.textContent = this.playName;
+        elem.appendChild(playNameEl);
+
+        const playIDEl = document.createElement('div');
+        playIDEl.id = this.playID;
+        elem.appendChild(playIDEl);
+
+        document.body.appendChild(elem);
+    }
+}
+
+customElements.define('dev-playlist', Playlist);
+
+const elemCreator = new Playlist();
+
+const json = [
+    { playName: 'Плейлист 1', playID: '123' }
+];
+
+json.forEach((a) => {
+    elemCreator.createElem({ playName: a.playName, playID: a.playID });
+});
