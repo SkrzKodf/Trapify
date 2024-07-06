291e6b4ffd0fe937f5c483f7a951e72c8f8ee137
John Doe <email@example.com>
2024-07-06 20:40:27 +0300

    Пробный поисковик оформленный, но без картинок

--- frontend/gulp/js/script.js
+++ frontend/gulp/js/script.js
@@ -1,9 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', (event) => {
+  const searchLink = document.getElementById('searchLink');
+  const searchBox = document.getElementById('searchBox');
 
-    var searchButton = document.getElementById('searchLink');
-    var searchBox = document.querySelector('.search-box');
-
-    searchButton.addEventListener('click', function() {
-      searchBox.style.display = searchBox.style.display === 'block' ? 'none' : 'block';
-    });
+  searchLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    // Переключаем видимость поискового блока
+    if (searchBox.style.display === 'none' || !searchBox.style.display) {
+      searchBox.style.display = 'block';
+    } else {
+      searchBox.style.display = 'none';
+    }
   });
+});
+// document.addEventListener('DOMContentLoaded', function() {
+
+//     var searchButton = document.getElementById('searchLink');
+//     var searchBox = document.querySelector('.search-box');
+
+//     searchButton.addEventListener('click', function() {
+//       searchBox.style.display = searchBox.style.display === 'block' ? 'none' : 'block';
+//     });
+
+    
+//   });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var header = document.getElementById('headId'); 
+    var headerHeight = header.offsetHeight;
+    var elementToOffset = document.getElementById('searchBox');; 
+    elementToOffset.style.marginTop = headerHeight + 'px';
+});
